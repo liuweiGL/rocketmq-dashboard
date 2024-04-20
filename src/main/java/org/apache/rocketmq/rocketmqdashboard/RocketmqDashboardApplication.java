eb512a8198e4154afe11a9e137bdbbefa30eae87
liuwei <wei2460@qq.com>
2024-04-21 05:42:59 +0800

    update

---
+++ src/main/java/org/apache/rocketmq/rocketmqdashboard/RocketmqDashboardApplication.java
@@ -0,0 +1,13 @@
+package org.apache.rocketmq.rocketmqdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketmqDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/apache/rocketmq/rocketmqdashboard/RocketmqDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.apache.rocketmq.rocketmqdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
